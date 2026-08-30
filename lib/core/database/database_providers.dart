92d61d900da599131beda45eb893777599cb611c
Sachidanand Gond <srkashyap126@outlook.com>
2026-08-30 10:54:05 +0530

    feat(db): register backupServiceProvider in database_providers

--- lib/core/database/database_providers.dart
+++ lib/core/database/database_providers.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../core/database/database_service.dart';
+import 'backup_service.dart';
+import 'database_service.dart';
 
 /// Singleton provider for the DatabaseService.
 final databaseProvider = Provider<DatabaseService>((ref) {
   return DatabaseService.instance;
+});
+
+/// Provider for the BackupService singleton.
+final backupServiceProvider = Provider<BackupService>((ref) {
+  final db = ref.watch(databaseProvider);
+  return BackupService(databaseService: db);
 });
